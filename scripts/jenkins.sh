4f74a59189aca5c1998342deae5b8bb1528369e3
Tim Koopmans <tim.koops@gmail.com>
2016-08-09 15:41:22 +1000

    add some response time and error rate targets

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -25,8 +25,20 @@ done
 flood_report=$(curl --silent --user $FLOOD_API_TOKEN: https://api.flood.io/floods/$flood_uuid/report | \
   /tmp/jq -r ".summary")
 
+error_rate=$(curl --silent --user $FLOOD_API_TOKEN: https://api.flood.io/floods/$flood_uuid | jq -r .error_rate)
+response_time=$(curl --silent --user $FLOOD_API_TOKEN: https://api.flood.io/floods/$flood_uuid | jq -r .response_time)
+
 echo
 echo "[$(date +%FT%T)+00:00] Detailed results at https://flood.io/$flood_uuid"
 echo "---"
 echo "$flood_report"
 
+if [ "$error_rate" -gt "0" ]; then
+  echo "Flood test failed with error rate $error_rate%"
+  exit 1
+fi
+
+if [ "$response_time" -gt "3000" ]; then
+  echo "Flood test failed with response time $response_time > 3000ms"
+  exit 2
+fi
