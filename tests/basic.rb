34afe4774812cb285b6a8849e86ba7763e1cd5c5
Tim Koopmans <tim.koops@gmail.com>
2016-09-18 21:22:30 -0500

    ignore jtl

--- tests/basic.rb
+++ tests/basic.rb
@@ -1,25 +1,11 @@
 require 'ruby-jmeter'
 
 test do
-  defaults  domain: 'localhost',
-            port: 8008,
-            proxy_host: 'localhost',
-            proxy_port: 8080
+  defaults  domain: 'localhost', port: 8008
 
   threads count: 1, loops: 1 do
-    random_timer 100, 200
 
-    with_user_agent :iphone
-    with_json
+    get url: '/api'
 
-    get name: 'entry point', url: '/api' do
-      assert json: '.status', value: 'OK'
-    end
-
-    post name: 'oauth', url: '/api/oauth',
-      fill_in: {
-        username: 'Michel Rosen',
-        password: 4141414141
-      }
   end
 end.run(path: '/usr/share/jmeter/bin/')
