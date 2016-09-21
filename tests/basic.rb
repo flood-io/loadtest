4b8157d3db24a3e15776906e1d80383d632ffef3
Tim Koopmans <tim.koops@gmail.com>
2016-09-20 22:39:05 -0500

    basic load test

--- tests/basic.rb
+++ tests/basic.rb
@@ -1,12 +1,12 @@
 require 'ruby-jmeter'
 
 test do
-  defaults  domain: 'flooded.io', port: 80
+  defaults  domain: 'flooded.io', port: 443, protocol: 'https'
 
-  threads count: 10, loops: 5 do
+  threads count: 10, loops: 5, rampup: 30 do
+    random_timer 1000, 2000
 
     get url: '/api'
 
   end
-end.flood ENV['FLOOD_API_TOKEN'], privacy_flag: 'public',
-
+end.flood ENV['FLOOD_API_TOKEN'], privacy: 'public'
