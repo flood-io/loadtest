c8c6c28df9923bb0e069f22ed619a0de22f4ae3b
Tricentis Flood <accounts@flood.io>
2020-06-09 03:16:05 +0000

    format document

--- scripts/verify.ps1
+++ scripts/verify.ps1
@@ -8,37 +8,37 @@ $bytes = [System.Text.Encoding]::ASCII.GetBytes($access_token)
 $base64 = [System.Convert]::ToBase64String($bytes)
 $basicAuthValue = "Basic $base64"
 $headers = @{
-    'Authorization' = $basicAuthValue
+  'Authorization' = $basicAuthValue
 }
 
 # Retrieve the Grid ID that we will be using.
 try {
     
-    $uri = "$api_url/floods/$flood_uuid"
-    $responseGrid = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
-    $outGridID = $responseGrid._embedded.grids[0].uuid
-    Write-Output ">> Grid ID is: $outGridID"
+  $uri = "$api_url/floods/$flood_uuid"
+  $responseGrid = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
+  $outGridID = $responseGrid._embedded.grids[0].uuid
+  Write-Output ">> Grid ID is: $outGridID"
 
 }
 catch {
-    $responseBody = ""
-    $errorMessage = $_.Exception.Message
-    if (Get-Member -InputObject $_.Exception -Name 'Response') {
-        write-output $_.Exception.Response
+  $responseBody = ""
+  $errorMessage = $_.Exception.Message
+  if (Get-Member -InputObject $_.Exception -Name 'Response') {
+    write-output $_.Exception.Response
        
-        try {
-            $result = $_.Exception.Response.GetResponseStream()
-            $reader = New-Object System.IO.StreamReader($result, [System.Text.Encoding]::ASCII)
-            $reader.BaseStream.Position = 0
-            $reader.DiscardBufferedData()
-            $responseBody = $reader.ReadToEnd();
-            Write-Output "response body: $responseBody"
-        }
-        catch {
-            Throw "An error occurred while calling REST method at: $uri. Error: $errorMessage. Cannot get more information."
-        }
+    try {
+      $result = $_.Exception.Response.GetResponseStream()
+      $reader = New-Object System.IO.StreamReader($result, [System.Text.Encoding]::ASCII)
+      $reader.BaseStream.Position = 0
+      $reader.DiscardBufferedData()
+      $responseBody = $reader.ReadToEnd();
+      Write-Output "response body: $responseBody"
     }
-    Throw "An error occurred while calling REST method at: $uri. Error: $errorMessage. Response body: $responseBody"
+    catch {
+      Throw "An error occurred while calling REST method at: $uri. Error: $errorMessage. Cannot get more information."
+    }
+  }
+  Throw "An error occurred while calling REST method at: $uri. Error: $errorMessage. Response body: $responseBody"
 
 }
 
@@ -46,91 +46,91 @@ catch {
 write-output ">> Waiting for Grid ($outGridID) to start ..."
 do {
 
-    $uri = "$api_url/grids/$outGridID"
-    $responseStatus1 = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
-    $currentGridStatus = $responseStatus1.status
+  $uri = "$api_url/grids/$outGridID"
+  $responseStatus1 = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
+  $currentGridStatus = $responseStatus1.status
 
-    if($currentGridStatus -eq "started"){
-        write-output ">> The Grid has successfully started."
-    }
+  if ($currentGridStatus -eq "started") {
+    write-output ">> The Grid has successfully started."
+  }
 
-    if($currentGridStatus -eq "starting"){
-        Start-Sleep -Seconds 10
-    }
+  if ($currentGridStatus -eq "starting") {
+    Start-Sleep -Seconds 10
+  }
 
-}while($currentGridStatus -eq "starting")
+}while ($currentGridStatus -eq "starting")
 
 #Wait for the Flood to start from the QUEUED status successfully
 write-output ">> Waiting for the Flood ($flood_uuid) to start ..."
-do{
+do {
 
-    $uri = "$api_url/floods/$flood_uuid"
-    $responseStatus = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
-    $currentFloodStatus = $responseStatus.status
+  $uri = "$api_url/floods/$flood_uuid"
+  $responseStatus = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
+  $currentFloodStatus = $responseStatus.status
 
-    if($currentFloodStatus -eq "running"){
-        write-output ">> The Flood has started."
-    }
+  if ($currentFloodStatus -eq "running") {
+    write-output ">> The Flood has started."
+  }
 
-    if($currentFloodStatus -eq "queued"){
-        Start-Sleep -Seconds 10
-    }
+  if ($currentFloodStatus -eq "queued") {
+    Start-Sleep -Seconds 10
+  }
 
-} while($currentFloodStatus -eq "queued")
+} while ($currentFloodStatus -eq "queued")
 
 # Wait for the Flood to complete and be in FINISHED status.
 write-output ">> Waiting for the Flood ($flood_uuid) to complete ..."
-do{
+do {
 
-    $uri = "$api_url/floods/$flood_uuid"
-    $responseStatus = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
-    $currentFloodStatus = $responseStatus.status
+  $uri = "$api_url/floods/$flood_uuid"
+  $responseStatus = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
+  $currentFloodStatus = $responseStatus.status
 
-    if($currentFloodStatus -eq "finished"){
-        write-output ">> The Flood has finished."
-    }
+  if ($currentFloodStatus -eq "finished") {
+    write-output ">> The Flood has finished."
+  }
 
-    if($currentFloodStatus -eq "running"){
-        Start-Sleep -Seconds 10
-    }
+  if ($currentFloodStatus -eq "running") {
+    Start-Sleep -Seconds 10
+  }
 
-}while($currentFloodStatus -eq "running")
+}while ($currentFloodStatus -eq "running")
 
 #Retrieve the mean error rate for the Flood to use for our simple SLA verification.
 try {
     
-    $uri = "$api_url/floods/$flood_uuid/report"
-    $responseReport = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
-    $outMeanErrorRate = $responseReport.mean_error_rate
-    Write-Output ">> Mean Error Rate is: $outMeanErrorRate"
+  $uri = "$api_url/floods/$flood_uuid/report"
+  $responseReport = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
+  $outMeanErrorRate = $responseReport.mean_error_rate
+  Write-Output ">> Mean Error Rate is: $outMeanErrorRate"
 
 }
 catch {
-    $responseBody = ""
-    $errorMessage = $_.Exception.Message
-    if (Get-Member -InputObject $_.Exception -Name 'Response') {
-        write-output $_.Exception.Response
+  $responseBody = ""
+  $errorMessage = $_.Exception.Message
+  if (Get-Member -InputObject $_.Exception -Name 'Response') {
+    write-output $_.Exception.Response
        
-        try {
-            $result = $_.Exception.Response.GetResponseStream()
-            $reader = New-Object System.IO.StreamReader($result, [System.Text.Encoding]::ASCII)
-            $reader.BaseStream.Position = 0
-            $reader.DiscardBufferedData()
-            $responseBody = $reader.ReadToEnd();
-            Write-Output "response body: $responseBody"
-        }
-        catch {
-            Throw "An error occurred while calling REST method at: $uri. Error: $errorMessage. Cannot get more information."
-        }
+    try {
+      $result = $_.Exception.Response.GetResponseStream()
+      $reader = New-Object System.IO.StreamReader($result, [System.Text.Encoding]::ASCII)
+      $reader.BaseStream.Position = 0
+      $reader.DiscardBufferedData()
+      $responseBody = $reader.ReadToEnd();
+      Write-Output "response body: $responseBody"
     }
-    Throw "An error occurred while calling REST method at: $uri. Error: $errorMessage. Response body: $responseBody"
+    catch {
+      Throw "An error occurred while calling REST method at: $uri. Error: $errorMessage. Cannot get more information."
+    }
+  }
+  Throw "An error occurred while calling REST method at: $uri. Error: $errorMessage. Response body: $responseBody"
 
 }
 
 #Our SLA SUCCESS criteria is that no errors are observed for this test.
-if($outMeanErrorRate -eq "0"){
-    write-output ">> SUCCESS - the Flood returned no errors or failed transactions."
+if ($outMeanErrorRate -eq "0") {
+  write-output ">> SUCCESS - the Flood returned no errors or failed transactions."
 }
 else {
-    Write-Error ">> FAILED - the Flood returned errors or failed transactions."
+  Write-Error ">> FAILED - the Flood returned errors or failed transactions."
 }
